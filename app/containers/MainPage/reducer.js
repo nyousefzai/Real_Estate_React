18bc6b3784c90321b5410ae80294681655ec2740
Navid_Dev <navid22y@gmail.com>
2019-02-09 00:21:18 -0800

    adding reducer

--- app/containers/MainPage/reducer.js
+++ app/containers/MainPage/reducer.js
@@ -5,14 +5,46 @@
  */
 
 import { fromJS } from 'immutable';
-import { DEFAULT_ACTION } from './constants';
+import {
+  LOAD_EVENTS,
+  LOAD_EVENTS_ERROR,
+  LOAD_EVENTS_SUCCESS,
+  LOAD_FEATURED_EVENTS,
+  LOAD_FEATURED_EVENTS_ERROR,
+  LOAD_FEATURED_EVENTS_SUCCESS,
+} from './constants';
 
-export const initialState = fromJS({});
+export const initialState = fromJS({
+  featuredEvents: [],
+  events: [],
+  loading: false,
+  loadingFeatured: false,
+  error: false,
+  errorFeatured: false,
+});
 
 function mainPageReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case LOAD_EVENTS:
+      return state.set('loading', true).set('error', false);
+    case LOAD_EVENTS_SUCCESS:
+      return state
+        .set('loading', false)
+        .set('error', false)
+        .set('events', action.events);
+    case LOAD_EVENTS_ERROR:
+      return state.set('loading', false).set('error', action.error);
+    case LOAD_FEATURED_EVENTS:
+      return state.set('loadingFeatured', true).set('errorFeatured', false);
+    case LOAD_FEATURED_EVENTS_ERROR:
+      return state
+        .set('loadingFeatured', false)
+        .set('errorFeatured', action.error);
+    case LOAD_FEATURED_EVENTS_SUCCESS:
+      return state
+        .set('loadingFeatured', false)
+        .set('errorFeatured', false)
+        .set('featuredEvents', action.featuredEvents);
     default:
       return state;
   }
