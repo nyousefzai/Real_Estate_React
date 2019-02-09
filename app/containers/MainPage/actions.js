2ce7bde39c75151536072f6c6796aa3f504c7998
Navid_Dev <navid22y@gmail.com>
2019-02-08 23:48:28 -0800

    adding some actions

--- app/containers/MainPage/actions.js
+++ app/containers/MainPage/actions.js
@@ -4,10 +4,58 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import {
+  LOAD_EVENTS,
+  LOAD_EVENTS_ERROR,
+  LOAD_EVENTS_SUCCESS,
+  LOAD_FEATURED_EVENTS,
+  LOAD_FEATURED_EVENTS_ERROR,
+  LOAD_FEATURED_EVENTS_SUCCESS,
+} from './constants';
 
-export function defaultAction() {
+export function loadEvent(tenentID, skip, take, searchTerm) {
   return {
-    type: DEFAULT_ACTION,
+    type: LOAD_EVENTS,
+    tenentID,
+    skip,
+    take,
+    searchTerm,
+  };
+}
+
+export function loadEventSuccess(events) {
+  return {
+    type: LOAD_EVENTS_SUCCESS,
+    events,
+  };
+}
+
+export function loadEventError(error) {
+  return {
+    type: LOAD_EVENTS_ERROR,
+    error,
+  };
+}
+
+export function loadFeaturedEvent(tenentID, skip, take) {
+  return {
+    type: LOAD_FEATURED_EVENTS,
+    tenentID,
+    skip,
+    take,
+  };
+}
+
+export function loadFeaturedEventSuccess(featuredEvents) {
+  return {
+    type: LOAD_FEATURED_EVENTS_SUCCESS,
+    featuredEvents,
+  };
+}
+
+export function loadFeaturedEventError(error) {
+  return {
+    type: LOAD_FEATURED_EVENTS_ERROR,
+    error,
   };
 }
--- app/containers/MainPage/constants.js
+++ app/containers/MainPage/constants.js
@@ -3,5 +3,13 @@
  * MainPage constants
  *
  */
+// Actions for featured events
+export const LOAD_FEATURED_EVENTS = 'app/MainPage/LOAD_FEATURED_EVENTS';
+export const LOAD_FEATURED_EVENTS_SUCCESS =
+  'app/MainPage/LOAD_FEATURED_EVENTS_SUCCESS';
+export const LOAD_FEATURED_EVENTS_ERROR = 'app/LOAD_FEATURED_EVENTS_ERROR';
 
-export const DEFAULT_ACTION = 'app/MainPage/DEFAULT_ACTION';
+// Actions for reguler events
+export const LOAD_EVENTS = 'app/MainPage/LOAD_EVENTS';
+export const LOAD_EVENTS_SUCCESS = 'app/MainPage/LOAD_EVENTS_SUCCESS';
+export const LOAD_EVENTS_ERROR = 'app/LOAD_EVENTS_ERROR';
